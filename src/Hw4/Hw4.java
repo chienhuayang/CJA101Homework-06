d7a6646ff0384ebce1a388e113d1096e54dccfda
chienhuayang <109363969+chienhuayang@users.noreply.github.com>
2025-03-19 23:09:21 +0800

    Update Hw4.java

--- src/Hw4/Hw4.java
+++ src/Hw4/Hw4.java
@@ -4,12 +4,12 @@ import java.util.Scanner;
 
 public class Hw4 {
 	public static void main(String[] args) {
-//		hw1();
-//		hw2();
-//		hw3();
-//		hw4();
+		hw1();
+		hw2();
+		hw3();
+		hw4();
 		hw5();
-//		hw6();
+		hw6();
 	}
 
 	public static void hw1() {
